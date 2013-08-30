7ef8ddc79fb8ef39f650821769c392db4bf54aca
GeertvanHorrik <geert@catenalogic.com>
2013-08-30 20:50:38 +0200

    Small improvements to Configuration class

--- src/Orchestra.Shell.NET40/Models/Configuration.cs
+++ src/Orchestra.Shell.NET40/Models/Configuration.cs
@@ -1,11 +1,30 @@
-﻿namespace Orchestra.Models
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Configuration.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2013 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orchestra.Models
 {
     /// <summary>
     /// The Configuration class contains the configurable items for the Orchestra Shell.
     /// </summary>
     public class Configuration
     {
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Configuration"/> class.
+        /// </summary>
+        public Configuration()
+        {
+            HelpTabText = "Help";
+            HelpGroupText = "Help";
+            HelpButtonText = "Help";
+        }
+        #endregion
 
+        #region Properties
         /// <summary>
         /// Gets or sets the name of the 'about' tab.
         /// </summary>
@@ -29,5 +48,6 @@
         /// The help button text.
         /// </value>
         public string HelpButtonText { get; set; }
+        #endregion
     }
 }
