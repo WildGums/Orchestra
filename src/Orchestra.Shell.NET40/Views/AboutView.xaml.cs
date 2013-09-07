9006002016916e51f27f8cadec319a13c68a098c
MarkoVee <marko.vermeer@mc-solutions.nl>
2013-09-07 16:04:41 +0200

    Made the AboutWindow image configarable the same way as the splashscreen's image

--- src/Orchestra.Shell.NET40/Views/AboutView.xaml.cs
+++ src/Orchestra.Shell.NET40/Views/AboutView.xaml.cs
@@ -1,5 +1,9 @@
 ﻿namespace Orchestra.Views
 {
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Windows.Media.Imaging;
     using Catel.Windows;
 
     using ViewModels;
@@ -9,6 +13,9 @@
     /// </summary>
     public partial class AboutView : DataWindow
     {
+        private const string AboutImageLocation = "Resources\\Images\\About.png";
+        private const string AboutImageFallbackLocation = "/Orchestra.Shell;component/Resources/Images/About.png";
+
         /// <summary>
         /// Initializes a new instance of the <see cref="AboutView"/> class.
         /// </summary>
@@ -26,6 +33,31 @@
             : base(viewModel, DataWindowMode.Custom)
         {
             InitializeComponent();
+            InitializeAboutWindow();
+        }
+
+        /// <summary>
+        /// Initializes the splash screen.
+        /// </summary>
+        private void InitializeAboutWindow()
+        {
+            var directory = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+
+            try
+            {
+                string firstAttemptFile = Path.Combine(directory, AboutImageLocation);
+                if (File.Exists(firstAttemptFile))
+                {
+                    aboutImage.Source = new BitmapImage(new Uri(firstAttemptFile, UriKind.Absolute));
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                // Swallow exception
+            }
+
+            aboutImage.Source = new BitmapImage(new Uri(AboutImageFallbackLocation, UriKind.RelativeOrAbsolute));
         }
     }
 }
