799e802910c6366eb1c69adbe158dd6f717ed544
Maxim <mkhomutov@gmail.com>
2013-09-26 14:49:41 +0400

    Added ability to send message when module is loaded

---
+++ src/Orchestra.Library.NET40/Messages/ModuleInitializedMessage.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ModuleInitialized.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2013 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Orchestra.Messages
+{
+    /// <summary>
+    /// The message, which sent when the module has been initialized.
+    /// </summary>
+    public class ModuleInitializedMessage
+    {
+        /// <summary>
+        /// The name of module
+        /// </summary>
+        public string ModuleName { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="moduleName"></param>
+        public ModuleInitializedMessage(string moduleName)
+        {
+            ModuleName = moduleName;
+        }
+    }
+}
