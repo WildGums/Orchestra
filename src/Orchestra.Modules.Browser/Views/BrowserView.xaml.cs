dcc9619d001905cac178bcd9378e09de07f0893b
GeertvanHorrik <geert@catenalogic.com>
2013-10-12 11:16:42 +0200

    UserAgent of WebBrowser module is now set to IE9 to prevent compatibility issues with some websites

--- src/Orchestra.Modules.Browser/Views/BrowserView.xaml.cs
+++ src/Orchestra.Modules.Browser/Views/BrowserView.xaml.cs
@@ -1,22 +1,27 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="BrowserView.xaml.cs" company="Orchestra development team">
-//   Copyright (c) 2008 - 2012 Orchestra development team. All rights reserved.
+//   Copyright (c) 2008 - 2013 Orchestra development team. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+
 namespace Orchestra.Modules.Browser.Views
 {
-    using Catel;
+    using System.Windows.Navigation;
     using Catel.IoC;
     using Catel.Messaging;
+    using Orchestra.Modules.Browser.ViewModels;
     using Orchestra.Views;
-    using ViewModels;
 
     /// <summary>
     /// Interaction logic for BrowserView.xaml.
     /// </summary>
     public partial class BrowserView : DocumentView
     {
+        #region Constants
+        private const string UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)";
+        #endregion
+
         #region Constructors
         /// <summary>
         /// Initializes a new instance of the <see cref="BrowserView"/> class.
@@ -24,7 +29,7 @@ namespace Orchestra.Modules.Browser.Views
         public BrowserView()
         {
             InitializeComponent();
-        }        
+        }
         #endregion
 
         #region Methods
@@ -48,7 +53,7 @@ namespace Orchestra.Modules.Browser.Views
 
         private void OnBrowse(string url)
         {
-            webBrowser.Navigate(url);
+            webBrowser.Navigate(url, null, null, string.Format("User-Agent: {0}", UserAgent));
         }
         #endregion
     }
