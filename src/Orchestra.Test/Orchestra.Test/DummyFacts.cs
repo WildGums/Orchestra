9a8c408ed8e1ea1465d069e06e6467a0c10b0e97
GeertvanHorrik <geert@catenalogic.com>
2014-06-17 15:15:59 +0200

    Added dummy tests

---
+++ src/Orchestra.Test/Orchestra.Test/DummyFacts.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DummyFacts.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orchestra.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DummyFacts
+    {
+        #region Methods
+        [TestMethod]
+        public void DummyTest()
+        {
+        }
+        #endregion
+    }
+}
