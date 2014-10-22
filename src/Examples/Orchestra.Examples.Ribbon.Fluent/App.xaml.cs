0c8b0b3409eb77275fea1f98244d7bc44a9c3ff7
GeertvanHorrik <geert@catenalogic.com>
2014-10-22 20:15:23 +0200

    Added fluent ribbon example shell

--- src/Examples/Orchestra.Examples.Ribbon.Fluent/App.xaml.cs
+++ src/Examples/Orchestra.Examples.Ribbon.Fluent/App.xaml.cs
@@ -1,16 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
-namespace Orchestra.Examples.Ribbon.Fluent
+
+namespace Orchestra.Examples.Ribbon
 {
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Input;
+    using Catel.IoC;
+    using Catel.Logging;
+    using Catel.MVVM;
+    using Orchestra.Services;
+    using Orchestra.Views;
+    using InputGesture = Catel.Windows.Input.InputGesture;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        #region Constants
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+        #endregion
+
+        #region Fields
+        private readonly DateTime _start;
+        private readonly Stopwatch _stopwatch;
+        private DateTime _end;
+        #endregion
+
+        #region Constructors
+        public App()
+        {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+            _start = DateTime.Now;
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStartup(StartupEventArgs e)
+        {
+#if DEBUG
+            LogManager.AddDebugListener(true);
+#endif
+
+            var serviceLocator = ServiceLocator.Default;
+            var shellService = serviceLocator.ResolveType<IShellService>();
+            shellService.CreateWithSplash<ShellWindow>(PreInitialize, InitializeCommands, PostInitialize);
+
+            _end = DateTime.Now;
+            _stopwatch.Stop();
+
+            Log.Info("Elapsed startup stopwatch time: {0}", _stopwatch.Elapsed);
+            Log.Info("Elapsed startup time: {0}", _end - _start);
+        }
+
+        private async Task PreInitialize()
+        {
+            Log.Info("Improving performance");
+
+            Catel.Data.ModelBase.DefaultSuspendValidationValue = true;
+            Catel.Windows.Controls.UserControl.DefaultCreateWarningAndErrorValidatorForViewModelValue = false;
+            Catel.Windows.Controls.UserControl.DefaultSkipSearchingForInfoBarMessageControlValue = true;
+        }
+
+        private async Task InitializeCommands(ICommandManager commandManager)
+        {
+            commandManager.CreateCommand("File.Open", new InputGesture(Key.O, ModifierKeys.Control), throwExceptionWhenCommandIsAlreadyCreated: false);
+            commandManager.CreateCommand("File.SaveToImage", new InputGesture(Key.I, ModifierKeys.Control), throwExceptionWhenCommandIsAlreadyCreated: false);
+            commandManager.CreateCommand("File.Print", new InputGesture(Key.P, ModifierKeys.Control), throwExceptionWhenCommandIsAlreadyCreated: false);
+            commandManager.CreateCommand("File.Exit", throwExceptionWhenCommandIsAlreadyCreated: false);
+
+            commandManager.CreateCommand("Help.About", throwExceptionWhenCommandIsAlreadyCreated: false);
+        }
+
+        private async Task PostInitialize()
+        {
+            Log.Info("Delay to show the splash screen");
+
+            Thread.Sleep(2500);
+        }
+        #endregion
     }
 }
---
+++ src/Orchestra.Shell/Orchestra.Shell.Ribbon.Fluent/Extensions/RibbonExtensions.cs
@@ -0,0 +1,110 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RibbonExtensions.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orchestra
+{
+    using System;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Media.Imaging;
+    using Catel;
+    using Catel.IoC;
+    using Catel.Logging;
+    using Catel.MVVM.Converters;
+    using Fluent;
+    using Services;
+    using FluentButton = Fluent.Button;
+
+    public static class RibbonExtensions
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        public static void AddAboutButton(this Ribbon ribbon)
+        {
+            Argument.IsNotNull(() => ribbon);
+
+            Log.Debug("Adding about button to ribbon");
+
+            var stackPanel = ribbon.EnsureHelpPaneStackPanel();
+
+            var aboutButton = CreateRibbonButton(GetImageUri("/Resources/Images/about.png"));
+            aboutButton.Click += (sender, e) =>
+            {
+                var aboutService = ServiceLocator.Default.ResolveType<IAboutService>();
+                aboutService.ShowAbout();
+            };
+            stackPanel.Children.Add(aboutButton);
+        }
+
+        [Obsolete("Use AddMinimizeAndMaximizeButtons() instead", true)]
+        public static void AddMinimizeMaximizeButton(this Ribbon ribbon)
+        {
+            AddMinimizeAndMaximizeButtons(ribbon);
+        }
+
+        public static void AddMinimizeAndMaximizeButtons(this Ribbon ribbon)
+        {
+            Argument.IsNotNull(() => ribbon);
+
+            Log.Debug("Adding minimize and maximize buttons to ribbon");
+
+            var stackPanel = ribbon.EnsureHelpPaneStackPanel();
+
+            var booleanToCollapsingVisibilityConverter = new BooleanToCollapsingVisibilityConverter();
+
+            var minimizeButton = CreateRibbonButton(GetImageUri("/Resources/Images/minimize.png"));
+            minimizeButton.SetBinding(FluentButton.VisibilityProperty, new Binding("IsMinimized")
+            {
+                Source = ribbon,
+                Converter = booleanToCollapsingVisibilityConverter,
+                ConverterParameter = false
+            });
+            minimizeButton.Click += (sender, e) => ribbon.IsMinimized = true;
+            stackPanel.Children.Add(minimizeButton);
+
+            var maximizeButton = CreateRibbonButton(GetImageUri("/Resources/Images/maximize.png"));
+            maximizeButton.SetBinding(FluentButton.VisibilityProperty, new Binding("IsMinimized")
+            {
+                Source = ribbon,
+                Converter = booleanToCollapsingVisibilityConverter,
+                ConverterParameter = true
+            });
+            maximizeButton.Click += (sender, e) => ribbon.IsMinimized = false;
+            stackPanel.Children.Add(maximizeButton);
+        }
+
+        private static StackPanel EnsureHelpPaneStackPanel(this Ribbon ribbon)
+        {
+            //var helpPaneContent = ribbon.HelpPaneContent as StackPanel;
+            StackPanel helpPaneContent = null;
+            if (helpPaneContent == null)
+            {
+                helpPaneContent = new StackPanel();
+                helpPaneContent.Orientation = Orientation.Horizontal;
+
+                //ribbon.HelpPaneContent = helpPaneContent;
+            }
+
+            return helpPaneContent;
+        }
+
+        private static Uri GetImageUri(string uri)
+        {
+            var finalUri = string.Format("/{0};component{1}", typeof (RibbonExtensions).Assembly.GetName().Name, uri);
+            return new Uri(finalUri, UriKind.RelativeOrAbsolute);
+        }
+
+        private static FluentButton CreateRibbonButton(Uri smallImageSource)
+        {
+            var ribbonButton = new FluentButton();
+
+            ribbonButton.Icon = new BitmapImage(smallImageSource);
+
+            return ribbonButton;
+        }
+    }
+}
