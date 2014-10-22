c2a6afd88a7ad393897fbde900a0d10472e7e5ef
GeertvanHorrik <geert@catenalogic.com>
2014-10-22 19:59:29 +0200

    Implemented Fluent ribbon shell

---
+++ src/Orchestra.Shell/Orchestra.Shell.Ribbon.Fluent/Services/Interfaces/IShellService.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IShellService.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orchestra.Services
+{
+    public partial interface IShellService
+    {
+    }
+}
