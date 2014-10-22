f0573ad86835df7afac4e7c1d5c8856e98bad97b
GeertvanHorrik <geert@catenalogic.com>
2014-10-22 19:44:10 +0200

    Added Fluent Ribbon example project (still empty shell though)

---
+++ src/Examples/Orchestra.Examples.Ribbon.Fluent/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Orchestra.Examples.Ribbon.Fluent
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
