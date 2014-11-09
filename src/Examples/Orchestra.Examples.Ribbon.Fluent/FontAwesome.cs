2a647331aa106b2bb86a0f1c808cbc4e14ba9d4c
GeertvanHorrik <geert@catenalogic.com>
2014-11-09 16:43:53 +0100

    Added fontawesome example

---
+++ src/Examples/Orchestra.Examples.Ribbon.Fluent/FontAwesome.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FontAwesome.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orchestra.Examples.Ribbon
+{
+    using System;
+    using System.Globalization;
+
+    /// <summary>
+    /// Class FontAwesome, characters can be found at http://fortawesome.github.io/Font-Awesome/cheatsheet/
+    /// </summary>
+    public static class FontAwesome
+    {
+        public static readonly string Refresh = GetCharacter("&#xf021;");
+        public static readonly string Windows = GetCharacter("&#xf17a;");
+
+        private static string GetCharacter(string unicode)
+        {
+            unicode = unicode.Replace("&#x", string.Empty);
+            unicode = unicode.TrimEnd(';');
+
+            var code = int.Parse(unicode, NumberStyles.AllowHexSpecifier);
+            return ((char) code).ToString();
+        }
+    }
+}
