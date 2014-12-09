2ebcdb6837b4eb8a1f66b8cbba42fdc4a47384aa
Dan Pristupov <danil@danil.cz>
2014-12-09 18:52:14 +0100

    Adds SaveFilePickerView

---
+++ src/Orchestra.Core/Orchestra.Core/ViewModels/SaveFilePickerViewModel.cs
@@ -0,0 +1,109 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SaveFilePickerViewModel.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orchestra.ViewModels
+{
+    using System.IO;
+    using Catel;
+    using Catel.MVVM;
+    using Catel.Services;
+
+    public class SaveFilePickerViewModel : ViewModelBase
+    {
+        #region Fields
+        private readonly IProcessService _processService;
+        private readonly ISaveFileService _saveFileService;
+        #endregion
+
+        #region Constructors
+        public SaveFilePickerViewModel(ISaveFileService saveFileService, IProcessService processService)
+        {
+            Argument.IsNotNull(() => saveFileService);
+            Argument.IsNotNull(() => processService);
+
+            _saveFileService = saveFileService;
+            _processService = processService;
+
+            OpenDirectory = new Command(OnOpenDirectoryExecute, OnOpenDirectoryCanExecute);
+            SelectFile = new Command(OnSelectFileExecute);
+        }
+        #endregion
+
+        #region Properties
+        public double LabelWidth { get; set; }
+
+        public string LabelText { get; set; }
+
+        public string SelectedFile { get; set; }
+
+        public string Filter { get; set; }
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// Gets the OpenDirectory command.
+        /// </summary>
+        public Command OpenDirectory { get; private set; }
+
+        /// <summary>
+        /// Method to check whether the OpenDirectory command can be executed.
+        /// </summary>
+        /// <returns><c>true</c> if the command can be executed; otherwise <c>false</c></returns>
+        private bool OnOpenDirectoryCanExecute()
+        {
+            if (string.IsNullOrWhiteSpace(SelectedFile))
+            {
+                return false;
+            }
+
+            var directory = Directory.GetParent(SelectedFile);
+            // Don't allow users to write text that they can "invoke" via our software
+            if (!directory.Exists)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Method to invoke when the OpenDirectory command is executed.
+        /// </summary>
+        private void OnOpenDirectoryExecute()
+        {
+            var directory = Directory.GetParent(SelectedFile);
+            _processService.StartProcess(directory.FullName);
+        }
+
+        /// <summary>
+        /// Gets the SelectFile command.
+        /// </summary>
+        public Command SelectFile { get; private set; }
+
+        /// <summary>
+        /// Method to invoke when the SelectFile command is executed.
+        /// </summary>
+        private void OnSelectFileExecute()
+        {
+            if (!string.IsNullOrEmpty(SelectedFile))
+            {
+                _saveFileService.InitialDirectory = Path.GetFullPath(SelectedFile);
+            }
+
+            if (!string.IsNullOrEmpty(Filter))
+            {
+                _saveFileService.Filter = Filter;
+            }
+
+            if (_saveFileService.DetermineFile())
+            {
+                SelectedFile = _saveFileService.FileName;
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/Orchestra.Core/Orchestra.Core/Views/SaveFilePickerView.xaml.cs
@@ -0,0 +1,77 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SaveFilePickerView.xaml.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Orchestra.Views
+{
+    using System.Windows;
+    using Catel.MVVM.Views;
+
+    /// <summary>
+    ///     Interaction logic for SaveFilePickerView.xaml
+    /// </summary>
+    public partial class SaveFilePickerView
+    {
+        static SaveFilePickerView()
+        {
+            typeof(SaveFilePickerView).AutoDetectViewPropertiesToSubscribe();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SaveFilePickerView"/> class.
+        /// </summary>
+        /// <remarks>This method is required for design time support.</remarks>
+        public SaveFilePickerView()
+        {
+            InitializeComponent();
+        }
+
+        #region Properties
+        [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewWins)]
+        public double LabelWidth
+        {
+            get { return (double)GetValue(LabelWidthProperty); }
+            set { SetValue(LabelWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelWidthProperty = DependencyProperty.Register("LabelWidth", typeof(double), typeof(SaveFilePickerView), new PropertyMetadata(125d));
+
+        [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewWins)] 
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelTextProperty =
+            DependencyProperty.Register("LabelText", typeof(string), typeof(SaveFilePickerView), new PropertyMetadata(string.Empty));
+
+        [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewWins)]
+        public string SelectedFile
+        {
+            get { return (string) GetValue(SelectedFileProperty); }
+            set { SetValue(SelectedFileProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedFile.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedFileProperty = DependencyProperty.Register("SelectedFile", typeof(string),
+            typeof(SaveFilePickerView), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        [ViewToViewModel(MappingType = ViewToViewModelMappingType.TwoWayViewWins)]
+        public string Filter
+        {
+            get { return (string) GetValue(FilterProperty); }
+            set { SetValue(FilterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedFile.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterProperty = DependencyProperty.Register("Filter", typeof(string),
+            typeof(SaveFilePickerView), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        #endregion
+    }
+}
