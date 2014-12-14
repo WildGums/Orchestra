8f1dc70bf86a9e69e3e039d23b0462228d69fd03
auz34 <andrewzavgorodniy@gmail.com>
2014-12-14 13:01:44 -0800

    Add initial implementation of commands we are implementing for each new project any way

--- src/Examples/Orchestra.Examples.Ribbon.Microsoft/ViewModels/RibbonViewModel.cs
+++ src/Examples/Orchestra.Examples.Ribbon.Microsoft/ViewModels/RibbonViewModel.cs
@@ -9,6 +9,8 @@ namespace Orchestra.Examples.Ribbon.ViewModels
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
 
     using Catel;
     using Catel.MVVM;
@@ -24,25 +26,41 @@ namespace Orchestra.Examples.Ribbon.ViewModels
         private readonly IUIVisualizerService _uiVisualizerService;
         private readonly IOpenFileService _openFileService;
         private readonly IRecentlyUsedItemsService _recentlyUsedItemsService;
+        private readonly IMessageService _messageService;
+        private readonly IProcessService _processService;
 
-        public RibbonViewModel(INavigationService navigationService, IUIVisualizerService uiVisualizerService,
-            ICommandManager commandManager, IRecentlyUsedItemsService recentlyUsedItemsService, IOpenFileService openFileService)
+        public RibbonViewModel(INavigationService navigationService, 
+            IUIVisualizerService uiVisualizerService,
+            ICommandManager commandManager, 
+            IRecentlyUsedItemsService recentlyUsedItemsService, 
+            IOpenFileService openFileService,
+            IMessageService messageService,
+            IProcessService processService)
         {
             Argument.IsNotNull(() => navigationService);
             Argument.IsNotNull(() => uiVisualizerService);
             Argument.IsNotNull(() => commandManager);
             Argument.IsNotNull(() => recentlyUsedItemsService);
             Argument.IsNotNull(() => openFileService);
+            Argument.IsNotNull(() => messageService);
+            Argument.IsNotNull(() => messageService);
 
             _navigationService = navigationService;
             _uiVisualizerService = uiVisualizerService;
             _recentlyUsedItemsService = recentlyUsedItemsService;
             _openFileService = openFileService;
+            _messageService = messageService;
+            _processService = processService;
 
             Help = new Command(OnHelpExecute);
             Open = new Command(this.OnOpenExecute);
             Exit = new Command(OnExitExecute);
             ShowKeyboardMappings = new Command(OnShowKeyboardMappingsExecute);
+
+            OpenRecentlyUsedItem = new Command<string>(OnOpenRecentlyUsedItemExecute);
+            UnpinItem = new Command<string>(OnUnpinItemExecute);
+            PinItem = new Command<string>(OnPinItemExecute);
+            OpenInExplorer = new Command<string>(OnOpenInExplorerExecute);
 
             RecentlyUsedItems = new List<RecentlyUsedItem>(_recentlyUsedItemsService.Items);
             PinnedItems = new List<RecentlyUsedItem>(_recentlyUsedItemsService.PinnedItems);
@@ -101,6 +119,86 @@ namespace Orchestra.Examples.Ribbon.ViewModels
         private async void OnShowKeyboardMappingsExecute()
         {
             await _uiVisualizerService.ShowDialog<KeyboardMappingsCustomizationViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the OpenRecentlyUsedItem command.
+        /// </summary>
+        public Command<string> OpenRecentlyUsedItem { get; private set; }
+
+        /// <summary>
+        /// Method to invoke when the OpenRecentlyUsedItem command is executed.
+        /// </summary>
+        private async void OnOpenRecentlyUsedItemExecute(string parameter)
+        {
+            var failed = false;
+
+            try
+            {
+                // TODO replace following line with actual load logic
+                failed = !File.Exists(parameter);
+            }
+            catch (Exception)
+            {
+                failed = true;
+            }
+
+            if (failed)
+            {
+                if (await _messageService.Show("The directory does not exist or has been removed. Would you like to remove it from the recently used list?", "Remove from recently used items?", MessageButton.YesNo) == MessageResult.Yes)
+                {
+                    var recentlyUsedItem = _recentlyUsedItemsService.Items.FirstOrDefault(x => string.Equals(x.Name, parameter));
+                    if (recentlyUsedItem != null)
+                    {
+                        _recentlyUsedItemsService.RemoveItem(recentlyUsedItem);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the Unpin command.
+        /// </summary>
+        public Command<string> UnpinItem { get; private set; }
+
+        /// <summary>
+        /// Method to invoke when the Unpin command is executed.
+        /// </summary>
+        private void OnUnpinItemExecute(string parameter)
+        {
+            _recentlyUsedItemsService.UnpinItem(parameter);
+        }
+
+        /// <summary>
+        /// Gets the Pin command.
+        /// </summary>
+        public Command<string> PinItem { get; private set; }
+
+        /// <summary>
+        /// Method to invoke when the Pin command is executed.
+        /// </summary>
+        private void OnPinItemExecute(string parameter)
+        {
+            _recentlyUsedItemsService.PinItem(parameter);
+        }
+
+        /// <summary>
+        /// Gets the OpenInExplorer command.
+        /// </summary>
+        public Command<string> OpenInExplorer { get; private set; }
+
+        /// <summary>
+        /// Method to invoke when the OpenInExplorer command is executed.
+        /// </summary>
+        private async void OnOpenInExplorerExecute(string parameter)
+        {
+            if (!File.Exists(parameter))
+            {
+                await _messageService.ShowWarning("The file doesn't seem to exist. Cannot open the project in explorer.");
+                return;
+            }
+
+            _processService.StartProcess(parameter);
         }
         #endregion
 
