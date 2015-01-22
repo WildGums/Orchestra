f3a5accafd63cc386c62b02f1b2050274cbe589d
Tanya Pristupova <zyrk@zyrk.net>
2015-01-22 13:09:17 +0100

    ORCOMP-50 Updates SystemInfoService

--- src/Orchestra.Core/Orchestra.Core/Services/SystemInfoService.cs
+++ src/Orchestra.Core/Orchestra.Core/Services/SystemInfoService.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="SystemInformationService.cs" company="Orchestra development team">
+// <copyright file="SystemInfoService.cs" company="Orchestra development team">
 //   Copyright (c) 2008 - 2015 Orchestra development team. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
@@ -12,10 +12,11 @@ namespace Orchestra.Services
     using System.Globalization;
     using System.Linq;
     using System.Management;
+    using Microsoft.Win32;
 
     internal class SystemInfoService : ISystemInfoService
     {
-        #region ISystemInformationService Members
+        #region ISystemInfoService Members
         public IEnumerable<KeyValuePair<string, string>> GetSystemInfo()
         {
             var wmi = new ManagementObjectSearcher("select * from Win32_OperatingSystem")
@@ -27,7 +28,6 @@ namespace Orchestra.Services
                 .Get()
                 .Cast<ManagementObject>()
                 .First();
-
 
             yield return new KeyValuePair<string, string>("User name:", Environment.UserName);
             yield return new KeyValuePair<string, string>("User domain name:", Environment.UserDomainName);
@@ -49,6 +49,16 @@ namespace Orchestra.Services
             yield return new KeyValuePair<string, string>("BusSpeedMHz:", GetObjectValue(cpu, "ExtClock"));
             yield return new KeyValuePair<string, string>("Number of cores:", GetObjectValue(cpu, "NumberOfCores"));
             yield return new KeyValuePair<string, string>("Number of logical processors:", GetObjectValue(cpu, "NumberOfLogicalProcessors"));
+
+            yield return new KeyValuePair<string, string>("Current culture:", CultureInfo.CurrentCulture.ToString());
+
+            yield return new KeyValuePair<string, string>("Installed .Net versions:", string.Empty);
+
+            var versions = GetVersionFromRegistry();
+            foreach (var version in versions)
+            {
+                yield return new KeyValuePair<string, string>(string.Empty, version);
+            }
         }
         #endregion
 
@@ -73,6 +83,63 @@ namespace Orchestra.Services
             }
 
             return finalValue;
+        }
+
+        private static IEnumerable<string> GetVersionFromRegistry()
+        {
+            // Opens the registry key for the .NET Framework entry. 
+            using (RegistryKey ndpKey =
+                RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").
+                    OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                // As an alternative, if you know the computers you will query are running .NET Framework 4.5  
+                // or later, you can use: 
+                // using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine,  
+                // RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    if (versionKeyName.StartsWith("v"))
+                    {
+                        var versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        var name = (string) versionKey.GetValue("Version", "");
+                        var sp = versionKey.GetValue("SP", "").ToString();
+                        var install = versionKey.GetValue("Install", "").ToString();
+                        if (sp != "" && install == "1")
+                        {
+                            yield return (versionKeyName + "  " + name + "  SP" + sp);
+                        }
+                        if (name != "")
+                        {
+                            continue;
+                        }
+                        foreach (string subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            var subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string) subKey.GetValue("Version", "");
+                            if (name != "")
+                            {
+                                sp = subKey.GetValue("SP", "").ToString();
+                            }
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (install == "") //no install info, must be later.
+                            {
+                                yield return (versionKeyName + "  " + name);
+                            }
+                            else
+                            {
+                                if (sp != "" && install == "1")
+                                {
+                                    yield return (versionKeyName + "  " + subKeyName + "  " + name + "  SP" + sp);
+                                }
+                                else if (install == "1")
+                                {
+                                    yield return (versionKeyName + "  " + subKeyName + "  " + name);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
         #endregion
     }
