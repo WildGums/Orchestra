55ab4d131de3775edcbd6d5b602cd2b1de32bd64
Maxim Khomutov <mkhomutov@gmail.com>
2015-04-20 13:15:02 +0300

    Updated Catel

--- src/Orchestra.Shell/Orchestra.Shell.TaskRunner/Orchestra.Shell.TaskRunner.Shared/Services/LogControlService.cs
+++ src/Orchestra.Shell/Orchestra.Shell.TaskRunner/Orchestra.Shell.TaskRunner.Shared/Services/LogControlService.cs
@@ -11,28 +11,63 @@ namespace Orchestra.Services
     using Catel.Logging;
     using Catel.Windows.Controls;
     using Catel.Windows.Threading;
+    using Orc.Controls;
 
     public class LogControlService : ILogControlService
     {
-        private readonly TraceOutputControl _traceOutputControl;
+        private readonly LogViewerControl _traceOutputControl;
 
         #region Constructors
-        public LogControlService(TraceOutputControl traceOutputControl)
+        public LogControlService(LogViewerControl traceOutputControl)
         {
             Argument.IsNotNull(() => traceOutputControl);
 
             _traceOutputControl = traceOutputControl;
             _traceOutputControl.Dispatcher.BeginInvoke(() =>
             {
-                _traceOutputControl.SelectedLevel = LogEvent.Info;
+                _traceOutputControl.ShowInfo = true;
+                _traceOutputControl.ShowError = false;
+                _traceOutputControl.ShowDebug = false;
+                _traceOutputControl.ShowWarning = false;
             });
         }
         #endregion
 
         public LogEvent SelectedLevel
         {
-            get { return _traceOutputControl.SelectedLevel; }
-            set { _traceOutputControl.SelectedLevel = value; }
+            get
+            {
+                LogEvent logEvent = 0;
+
+                if (_traceOutputControl.ShowDebug)
+                {
+                    logEvent |= LogEvent.Debug;
+                }
+
+                if (_traceOutputControl.ShowError)
+                {
+                    logEvent |= LogEvent.Error;
+                }
+
+                if (_traceOutputControl.ShowInfo)
+                {
+                    logEvent |= LogEvent.Info;
+                }
+
+                if (_traceOutputControl.ShowWarning)
+                {
+                    logEvent |= LogEvent.Warning;
+                }
+
+                return logEvent;
+            }
+            set
+            {
+                _traceOutputControl.ShowDebug = value.HasFlag(LogEvent.Debug);
+                _traceOutputControl.ShowError = value.HasFlag(LogEvent.Error);
+                _traceOutputControl.ShowInfo = value.HasFlag(LogEvent.Info);
+                _traceOutputControl.ShowWarning = value.HasFlag(LogEvent.Warning);
+            }
         }
 
         public void Clear()
