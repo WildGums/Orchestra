b68ec4c4a58fddccf4b9131f55cd13935561b4a8
GeertvanHorrik <geert@catenalogic.com>
2015-05-17 12:24:06 +0200

    Layout updates

--- src/Examples/Orchestra.Examples.Ribbon.Fluent/Orchestra.Examples.Ribbon.Fluent.Shared/Constants.cs
+++ src/Examples/Orchestra.Examples.Ribbon.Fluent/Orchestra.Examples.Ribbon.Fluent.Shared/Constants.cs
@@ -10,17 +10,17 @@ namespace Orchestra.Examples.Ribbon
     using System.Windows.Input;
     using InputGesture = Catel.Windows.Input.InputGesture;
 
-    public static class Commands
-    {
-        public static class Application
-        {
-            public const string Exit = "Application.Exit";
-            public static readonly InputGesture ExitInputGesture = new InputGesture(Key.F4, ModifierKeys.Alt);
+	public static class Commands
+	{
+		public static class Application
+		{
+			public const string Exit = "Application.Exit";
+			public static readonly InputGesture ExitInputGesture = new InputGesture(Key.F4, ModifierKeys.Alt);
 
-            public const string About = "Application.About";
-            public static readonly InputGesture AboutInputGesture = new InputGesture(Key.F1);
-        }
-    }
+			public const string About = "Application.About";
+			public static readonly InputGesture AboutInputGesture = new InputGesture(Key.F1);
+		}
+	}
 
     public static class Settings
     {
