bdd0f60f2fc13a71d2fe97c760b349c498f593f1
GeertvanHorrik <geert@catenalogic.com>
2015-11-30 13:35:41 +0100

    Added application watchers

---
+++ src/Orchestra.Shell/Orchestra.Shell.Shared/ApplicationWatchers/CloseApplicationWatcherBase.cs
@@ -0,0 +1,112 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CloseApplicationWatcherBase.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orchestra
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using Catel.Services;
+    using Catel.Threading;
+
+    public abstract class CloseApplicationWatcherBase : ApplicationWatcherBase
+    {
+        private static bool _isClosingConfirmed;
+        private static readonly IList<CloseApplicationWatcherBase> Watchers = new List<CloseApplicationWatcherBase>();
+
+        protected CloseApplicationWatcherBase()
+        {
+            Watchers.Add(this);
+
+            EnqueueShellActivatedAction(Subscribe);
+        }
+
+        private static async void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            if (e.Cancel)
+            {
+                return;
+            }
+
+            var window = sender as Window;
+            if (window == null)
+            {
+                return;
+            }
+
+            if (!_isClosingConfirmed)
+            {
+                e.Cancel = true;
+                await TaskHelper.Run(() => PerformClosingOperationsAsync(window), true);
+            }
+        }
+
+        private static async Task PerformClosingOperationsAsync(Window window)
+        {
+            _isClosingConfirmed = await ExecuteClosingAsync(watcher => watcher.PrepareClosingAsync()).ConfigureAwait(false);
+            if (!_isClosingConfirmed)
+            {
+                return;
+            }
+
+            _isClosingConfirmed = await ExecuteClosingAsync(watcher => watcher.ClosingAsync()).ConfigureAwait(false);
+            if (_isClosingConfirmed)
+            {
+                await DispatcherService.InvokeAsync(window.Close).ConfigureAwait(false);
+            }
+            else
+            {
+                NotifyClosingCanceled();
+            }
+        }
+
+        private static void NotifyClosingCanceled()
+        {
+            foreach (var watcher in Watchers)
+            {
+                watcher.ClosingCanceled();
+            }
+        }
+
+        private static async Task<bool> ExecuteClosingAsync(Func<CloseApplicationWatcherBase, Task<bool>> operation)
+        {
+            foreach (var watcher in Watchers)
+            {
+                if (!await operation(watcher).ConfigureAwait(false))
+                {
+                    NotifyClosingCanceled();
+
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected virtual void ClosingCanceled()
+        {
+            
+        }
+
+        protected virtual Task<bool> PrepareClosingAsync()
+        {
+            return TaskHelper<bool>.FromResult(true);
+        }
+
+        protected virtual Task<bool> ClosingAsync()
+        {
+            return TaskHelper<bool>.FromResult(true);
+        }
+
+        private static void Subscribe(Window window)
+        {
+            window.Closing += OnWindowClosing;
+        }
+    }
+}
---
+++ src/Orchestra.Shell/Orchestra.Shell.Shared/ApplicationWatchers/KeyPressWindowWatcher.cs
@@ -0,0 +1,170 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KeyPressWindowWatcher.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orchestra
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public class KeyPressWindowWatcher
+    {
+        private static readonly bool[] _heldDown = new bool[256];
+
+        private Action<KeyEventArgs> _keyDownHandler;
+        private Action<KeyEventArgs> _keyUpHandler;
+        private Action<KeyEventArgs> _previewKeyDownHandler;
+        private Action<KeyEventArgs> _previewKeyUpHandler;
+
+        public void WatchWindow(Window window)
+        {
+            window.PreviewKeyDown += OnPreviewKeyDown;
+            window.KeyDown += OnKeyDown;
+            window.PreviewKeyUp += OnPreviewKeyUp;
+            window.KeyUp += OnKeyUp;
+        }
+
+        public void UnWatchWindow(Window window)
+        {
+            window.PreviewKeyDown -= OnPreviewKeyDown;
+            window.KeyDown -= OnKeyDown;
+            window.PreviewKeyUp -= OnPreviewKeyUp;
+            window.KeyUp -= OnKeyUp;
+        }
+
+        public static bool IsKeyHeldDown(Key key)
+        {
+            var virtualKey = KeyInterop.VirtualKeyFromKey(key);
+            return _heldDown[virtualKey];
+        }
+
+        public static bool IsCtrlHeldDown()
+        {
+            return IsKeyHeldDown(Key.LeftCtrl) || IsKeyHeldDown(Key.RightCtrl);
+        }
+
+        public static bool IsShiftHeldDown()
+        {
+            return IsKeyHeldDown(Key.LeftShift) || IsKeyHeldDown(Key.RightShift);
+        }
+
+        public static bool IsAltHeldDown()
+        {
+            return IsKeyHeldDown(Key.LeftAlt) || IsKeyHeldDown(Key.RightAlt);
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            var virtualKey = KeyInterop.VirtualKeyFromKey(e.Key);
+            _heldDown[virtualKey] = false;
+
+            if (e.Handled)
+            {
+                return;
+            }
+
+            if (_keyUpHandler == null)
+            {
+                return;
+            }
+
+            var window = sender as Window;
+            if (window == null)
+            {
+                return;
+            }
+
+            _keyUpHandler(e);
+        }
+
+        private void OnPreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Handled)
+            {
+                return;
+            }
+
+            if (_previewKeyUpHandler == null)
+            {
+                return;
+            }
+
+            var window = sender as Window;
+            if (window == null)
+            {
+                return;
+            }
+
+            _previewKeyUpHandler(e);
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Handled)
+            {
+                return;
+            }
+
+            if (_keyDownHandler == null)
+            {
+                return;
+            }
+
+            var window = sender as Window;
+            if (window == null)
+            {
+                return;
+            }
+
+            _keyDownHandler(e);
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            var virtualKey = KeyInterop.VirtualKeyFromKey(e.Key);
+            _heldDown[virtualKey] = true;
+
+            if (e.Handled)
+            {
+                return;
+            }
+
+            if (_previewKeyDownHandler == null)
+            {
+                return;
+            }
+
+            var window = sender as Window;
+            if (window == null)
+            {
+                return;
+            }
+
+            _previewKeyDownHandler(e);
+        }
+
+        public void SetPreviewKeyDownHandler(Action<KeyEventArgs> handler)
+        {
+            _previewKeyDownHandler = handler;
+        }
+
+        public void SetKeyDownHandler(Action<KeyEventArgs> handler)
+        {
+            _keyDownHandler = handler;
+        }
+
+        public void SetPreviewKeyUpHandler(Action<KeyEventArgs> handler)
+        {
+            _previewKeyUpHandler = handler;
+        }
+
+        public void SetKeyUpHandler(Action<KeyEventArgs> handler)
+        {
+            _keyUpHandler = handler;
+        }
+    }
+}
