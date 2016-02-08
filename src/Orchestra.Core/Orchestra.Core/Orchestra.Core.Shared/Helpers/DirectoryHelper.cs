51e77ea3bd64d2b2a529f620e3311e406e400b59
GeertvanHorrik <geert@catenalogic.com>
2016-02-08 21:06:22 +0100

    (+) Added DirectoryHelper

---
+++ src/Orchestra.Core/Orchestra.Core/Orchestra.Core.Shared/Helpers/DirectoryHelper.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DirectoryHelper.cs" company="WildGums">
+//   Copyright (c) 2008 - 2016 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orchestra
+{
+    using System.IO;
+
+    public static class DirectoryHelper
+    {
+        public static void CopyDirectory(string sourceDirName, string destDirName, bool copySubDirs = true)
+        {
+            var dir = new DirectoryInfo(sourceDirName);
+            var dirs = dir.GetDirectories();
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException("Source directory does not exist or could not be found: " + sourceDirName);
+            }
+
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                var temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            if (copySubDirs)
+            {
+                foreach (var subdir in dirs)
+                {
+                    var temppath = Path.Combine(destDirName, subdir.Name);
+                    CopyDirectory(subdir.FullName, temppath);
+                }
+            }
+        }
+    }
+}
