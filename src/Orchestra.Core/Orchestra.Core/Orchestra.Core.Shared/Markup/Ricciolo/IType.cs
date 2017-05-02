96015a26ae48b25f5ba4e47ee26a77b42d5b0096
Geert van Horrik <geert@catenalogic.com>
2017-05-02 16:17:46 +0200

    Migrate code (StyleHelper & default style resource dictionaries) from Catel.Extensions.Controls => Orchestra

---
+++ src/Orchestra.Core/Orchestra.Core/Orchestra.Core.Shared/Markup/Ricciolo/IType.cs
@@ -0,0 +1,18 @@
+﻿#if NET
+
+#pragma warning disable 1591 // 1591 = missing xml
+
+namespace Orchestra.StylesExplorer.MarkupReflection
+{
+    /// <summary>
+    /// Interface rappresenting a DotNet type
+    /// </summary>
+    internal interface IType
+    {
+        string AssemblyQualifiedName { get; }
+        bool IsSubclassOf(IType type);
+        bool Equals(IType type);
+    }
+}
+
+#endif
---
+++ src/Orchestra.Core/Orchestra.Core/Orchestra.Core.Shared/Markup/Ricciolo/XmlBamlNode.cs
@@ -0,0 +1,18 @@
+#if NET
+
+#pragma warning disable 1591 // 1591 = missing xml
+
+using System.Xml;
+
+namespace Orchestra.StylesExplorer.MarkupReflection
+{
+    internal class XmlBamlNode
+    {
+        public virtual XmlNodeType NodeType
+        {
+            get { return XmlNodeType.None;}
+        }
+    }
+}
+
+#endif
