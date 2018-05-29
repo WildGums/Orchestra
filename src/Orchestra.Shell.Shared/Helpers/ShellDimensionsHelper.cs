87b9f278329789368fb8c56c8902c0c5520dc89e
Geert van Horrik <geert@catenalogic.com>
2018-05-29 21:40:11 +0200

    Only update dimension values when they make sense

--- src/Orchestra.Shell.Shared/Helpers/ShellDimensionsHelper.cs
+++ src/Orchestra.Shell.Shared/Helpers/ShellDimensionsHelper.cs
@@ -7,25 +7,25 @@
         public static void ApplyDimensions(Window window, FrameworkElement mainView)
         {
             var minHeight = mainView.MinHeight;
-            if (!double.IsNaN(minHeight))
+            if (!double.IsNaN(minHeight) && !double.IsInfinity(minHeight))
             {
                 window.SetCurrentValue(Window.MinHeightProperty, minHeight);
             }
 
             var maxHeight = mainView.MaxHeight;
-            if (!double.IsNaN(maxHeight))
+            if (!double.IsNaN(maxHeight) && !double.IsInfinity(maxHeight))
             {
                 window.SetCurrentValue(Window.MaxHeightProperty, maxHeight);
             }
 
             var minWidth = mainView.MinWidth;
-            if (!double.IsNaN(minWidth))
+            if (!double.IsNaN(minWidth) && !double.IsInfinity(minWidth))
             {
                 window.SetCurrentValue(Window.MinWidthProperty, minWidth);
             }
 
             var maxWidth = mainView.MaxWidth;
-            if (!double.IsNaN(maxWidth))
+            if (!double.IsNaN(maxWidth) && !double.IsInfinity(maxWidth))
             {
                 window.SetCurrentValue(Window.MaxWidthProperty, maxWidth);
             }
