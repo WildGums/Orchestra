79c758e1076104ea74fd21cf0302bd13fd448a91
Geert van Horrik <geert@catenalogic.com>
2019-11-07 00:05:21 +0100

    Fix some first chance exceptions

--- src/Orchestra.Examples.Ribbon.Fluent/Services/AboutInfoService.cs
+++ src/Orchestra.Examples.Ribbon.Fluent/Services/AboutInfoService.cs
@@ -15,7 +15,7 @@ namespace Orchestra.Examples.Ribbon.Services
     {
         public AboutInfo GetAboutInfo()
         {
-            var aboutInfo = new AboutInfo(new Uri("pack://application:,,,/Resources/Images/CompanyLogo.png", UriKind.RelativeOrAbsolute),
+            var aboutInfo = new AboutInfo(new Uri($"pack://application:,,,/{Catel.Reflection.AssemblyHelper.GetEntryAssembly().GetName().Name};component/Resources/Images/CompanyLogo.png", UriKind.RelativeOrAbsolute),
                 uriInfo: new UriInfo("https://www.catelproject.com", "Product website"));
 
             return aboutInfo;
