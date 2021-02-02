88f77dd2f242696fb1af1766e56a8c537258f898
Max <mkhomutov@gmail.com>
2021-02-02 13:50:43 +0300

    Revert "trying to fix test error on build server. change version number"
    This reverts commit 6d108c71d4abf142c4185aa8bea4224a37aea578.

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("WildGums")]
-[assembly: AssemblyVersion("6.2.2")]
-[assembly: AssemblyFileVersion("6.2.2")]
-[assembly: AssemblyInformationalVersion("6.2.2-alpha.1")]
+[assembly: AssemblyVersion("6.0.0")]
+[assembly: AssemblyFileVersion("6.0.0")]
+[assembly: AssemblyInformationalVersion("6.0.0-alpha.1")]
 [assembly: AssemblyCopyright("Copyright © WildGums 2010 - 2018")]
 
