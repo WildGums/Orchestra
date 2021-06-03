20bd9f4be50ec9528395d4cbc6f2efabbebb310a
Artem Saikin <art.saikin@gmail.com>
2021-06-03 15:32:59 +0300

    Added basic implementation for configuration backups (CS-191)

---
+++ src/Orchestra.Core/Services/ConfigurationBackupService.cs
@@ -0,0 +1,112 @@
+﻿namespace Orchestra.Services
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Catel;
+    using Catel.Configuration;
+    using Catel.Logging;
+    using Catel.Reflection;
+    using Catel.Services;
+    using MethodTimer;
+    using Orc.FileSystem;
+
+    public class ConfigurationBackupService : IConfigurationBackupService
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+        private readonly IConfigurationService _configurationService;
+        private readonly IAppDataService _appDataService;
+        private readonly IFileService _fileService;
+        private readonly IDirectoryService _directoryService;
+
+        public ConfigurationBackupService(IConfigurationService configurationService, IAppDataService appDataService, IFileService fileService, IDirectoryService directoryService)
+        {
+            Argument.IsNotNull(() => configurationService);
+            Argument.IsNotNull(() => appDataService);
+            Argument.IsNotNull(() => fileService);
+            Argument.IsNotNull(() => directoryService);
+
+            _configurationService = configurationService;
+            _appDataService = appDataService;
+            _fileService = fileService;
+            _directoryService = directoryService;
+        }
+
+        public int NumberOfBackups { get; protected set; } = 5;
+
+        public string BackupTimeStampFormat { get; set; } = "{0:yyyyMMdd_HHmmssfff}";
+
+        [Time]
+        public void Backup()
+        {
+            try
+            {
+                // Get configuration paths
+                // Catel.Reflection.ReflectionExtensions.GetFieldEx
+                var roamingConfigFilePathField = _configurationService.GetType().GetFieldEx("_roamingConfigFilePath", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                var localConfigFilePathField = _configurationService.GetType().GetFieldEx("_localConfigFilePath", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+                var roamingConfigFilePath = roamingConfigFilePathField.GetValue(_configurationService).ToString();
+                var localConfigFilePath = localConfigFilePathField.GetValue(_configurationService).ToString();
+
+                if (_fileService.Exists(roamingConfigFilePath))
+                {
+                    BackupConfiguration(roamingConfigFilePath, Catel.IO.ApplicationDataTarget.UserRoaming);
+                }
+                else
+                {
+                    Log.Info($"Configuration file not found on path {roamingConfigFilePath}, skipping backup");
+                }
+                if (_fileService.Exists(localConfigFilePath))
+                {
+                    BackupConfiguration(localConfigFilePath, Catel.IO.ApplicationDataTarget.UserLocal);
+                }
+                else
+                {
+                    Log.Info($"Configuration file not found on path {roamingConfigFilePath}, skipping backup");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Failed to create database backup");
+                throw;
+            }
+        }
+
+        private void BackupConfiguration(string configurationFilePath, Catel.IO.ApplicationDataTarget applicationDataTarget)
+        {
+            Log.Info($"Creating configuration backup, {applicationDataTarget}");
+
+            var configBackupFolderPath = Path.Combine(_appDataService.GetApplicationDataDirectory(Catel.IO.ApplicationDataTarget.UserRoaming), "backup\\config\\");
+            if (!_directoryService.Exists(configBackupFolderPath))
+            {
+                _directoryService.Create(configBackupFolderPath);
+            }
+
+            var backupConfigurationFiles = _directoryService.GetFiles(configBackupFolderPath, "configuration*").OrderBy(f => f).ToList();
+            if (backupConfigurationFiles.Count >= NumberOfBackups)
+            {
+                // Remove oldest backup
+                var filesToRemoveCount = 1 + NumberOfBackups - backupConfigurationFiles.Count;
+
+                for (int i = 0; i < filesToRemoveCount; i++)
+                {
+                    var backupFilePath = Path.Combine(configBackupFolderPath, backupConfigurationFiles[i]);
+                    _fileService.Delete(backupFilePath);
+                }
+            }
+
+            // Save current configuration
+            _fileService.Copy(configurationFilePath, Path.Combine(configBackupFolderPath, $"configuration.{string.Format(BackupTimeStampFormat, DateTime.Now)}.xml"));
+
+            Log.Info($"Created configration backup, {applicationDataTarget}");
+        }
+
+        [Time]
+        public async Task BackupAsync()
+        {
+            Backup();
+        }
+    }
+}
---
+++ src/Orchestra.Core/Services/Interfaces/IConfigurationBackupService.cs
@@ -0,0 +1,11 @@
+﻿namespace Orchestra.Services
+{
+    using System.Threading.Tasks;
+    using MethodTimer;
+
+    public interface IConfigurationBackupService
+    {
+        void Backup();
+        Task BackupAsync();
+    }
+}
