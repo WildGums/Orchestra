6afc87b7e9846edd92567c5a24d984ee56540d33
Artem Saikin <art.saikin@gmail.com>
2022-08-29 11:39:16 +0300

    Code improvements

--- src/Orchestra.Tests/Services/CloseApplicationWatcherBaseFacts.cs
+++ src/Orchestra.Tests/Services/CloseApplicationWatcherBaseFacts.cs
@@ -5,20 +5,41 @@
     using System.Reflection;
     using System.Threading;
     using System.Threading.Tasks;
+    using Catel;
     using NUnit.Framework;
 
     [TestFixture]
     public class CloseApplicationWatcherBaseFacts
     {
+        private const int OnWindowClosingWaitingTimeout = 5000;
+
         [TestCase]
         public async Task Verify_Closing_Allows_Cancel_When_Returning_False_Async()
         {
-            bool isWatcherCompleted = false;
-            using (var cts = new CancellationTokenSource(5000))
-            {
-                // tested object
-                var watcher = new TestCloseApplicationWatcher(true);
+            var watcher = new TestCloseApplicationWatcher(true);
+            await RunOnWindowClosingAndWaitForFinishAsync(watcher, OnWindowClosingWaitingTimeout);
 
+            Assert.IsTrue(watcher.IsClosingRun, "Closing did not run");
+            Assert.IsFalse(watcher.IsClosedRun, "Closed did run");
+        }
+
+        [TestCase]
+        public async Task Verify_Closing_Closed_Operations_Are_Executing_Async()
+        {
+            var watcher = new TestCloseApplicationWatcher(false);
+            await RunOnWindowClosingAndWaitForFinishAsync(watcher, OnWindowClosingWaitingTimeout);
+
+            Assert.IsTrue(watcher.IsClosingRun, "Closing did not run");
+            Assert.IsTrue(watcher.IsClosedRun, "Closed did not run");
+        }
+
+        private async Task RunOnWindowClosingAndWaitForFinishAsync(TestCloseApplicationWatcher watcher, int timeout)
+        {
+            Argument.IsNotNull(() => watcher);
+
+            bool isWatcherCompleted = false;
+            using (var cts = new CancellationTokenSource(timeout))
+            {
                 // Use a semaphore to prevent the [TestMethod] from returning prematurely.
                 using (var semaphore = await RunStaThreadAsync(() =>
                 {
@@ -52,56 +73,6 @@
                 }))
                 {
                     await semaphore.WaitAsync();
-
-                    Assert.IsTrue(watcher.IsClosingRun, "Closing did not run");
-                    Assert.IsFalse(watcher.IsClosedRun, "Closed did run");
-                }
-            }
-        }
-
-        [TestCase]
-        public async Task Verify_Closing_Closed_Operations_Are_Executing_Async()
-        {
-            bool isWatcherCompleted = false;
-            using (var cts = new CancellationTokenSource(5000))
-            {
-                // tested object
-                var watcher = new TestCloseApplicationWatcher(false);
-
-                // Use a semaphore to prevent the [TestMethod] from returning prematurely.
-                using (var semaphore = await RunStaThreadAsync(() =>
-                {
-                    var window = new System.Windows.Window();
-                    
-                    // access handler method
-                    var onWindowClosing = typeof(CloseApplicationWatcherBase).GetMethod("OnWindowClosing", BindingFlags.Static | BindingFlags.NonPublic);
-
-                    Assert.IsNotNull(onWindowClosing);
-
-                    var cancelEventArgs = new CancelEventArgs();
-                    var cancelEventArgsRetry = new CancelEventArgs();
-
-                    window.Closing += (sender, e) =>
-                    {
-                        onWindowClosing.Invoke(watcher, new object[] { window, cancelEventArgsRetry });
-                    };
-
-                    onWindowClosing.Invoke(watcher, new object[] { window, cancelEventArgs });
-
-                    while (!cts.IsCancellationRequested)
-                    {
-                        isWatcherCompleted = watcher.IsClosedRun && watcher.IsClosingRun;
-                        if (isWatcherCompleted)
-                        {
-                            break;
-                        }
-                    }
-                }))
-                {
-                    await semaphore.WaitAsync();
-
-                    Assert.IsTrue(watcher.IsClosingRun, "Closing did not run");
-                    Assert.IsTrue(watcher.IsClosedRun, "Closed did not run");
                 }
             }
         }
