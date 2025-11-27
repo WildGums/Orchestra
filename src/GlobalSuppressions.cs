8682ee778f4e53a83ec427c35cbf2228d560a679
Geert van Horrik <geert@catenalogic.com>
2025-11-27 11:58:34 +0100

    Maintenance

--- src/GlobalSuppressions.cs
+++ src/GlobalSuppressions.cs
@@ -4,7 +4,3 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF1010:Property '[property]' must notify when value changes.", Justification = "Don't enforce this")]
-[assembly: SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF1011:Implement INotifyPropertyChanged.", Justification = "Don't enforce this")]
-[assembly: SuppressMessage("Usage", "CA2255:The 'ModuleInitializer' attribute should not be used in libraries", Justification = "Used to register language resources and types", Scope = "member", Target = "~M:ModuleInitializer.Initialize")]
