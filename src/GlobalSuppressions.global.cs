dfc5d1b2f91905e8952d3b5e6178a65adbdd078d
Geert van Horrik <GeertvanHorrik@users.noreply.github.com>
2025-12-19 11:43:50 +0100

    GitHubSync update

--- src/GlobalSuppressions.global.cs
+++ src/GlobalSuppressions.global.cs
@@ -8,4 +8,3 @@
 [assembly: SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF1010:Property '[property]' must notify when value changes.", Justification = "Don't enforce this")]
 [assembly: SuppressMessage("WpfAnalyzers.DependencyProperties", "WPF1011:Implement INotifyPropertyChanged.", Justification = "Don't enforce this")]
 [assembly: SuppressMessage("Usage", "CA2255:The 'ModuleInitializer' attribute should not be used in libraries", Justification = "Used to register language resources and types")]
-
