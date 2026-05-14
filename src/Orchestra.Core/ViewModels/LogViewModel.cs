880c70ba2cf74b5ccf3408a2a3e0d90016cd32a2
Geert van Horrik <geert@catenalogic.com>
2026-05-14 16:43:42 +0200

    Update

--- src/Orchestra.Core/ViewModels/LogViewModel.cs
+++ src/Orchestra.Core/ViewModels/LogViewModel.cs
@@ -3,7 +3,7 @@
 using System;
 using Catel.MVVM;
 
-public class LogViewModel : ViewModelBase
+public partial class LogViewModel : ViewModelBase
 {
     public LogViewModel(IServiceProvider serviceProvider)
         : base(serviceProvider)
